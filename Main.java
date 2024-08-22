f8740a5946c7692397afbb7e46a96e6153b17cb8
Krishala Dangol <Krishaladangol16@gmail.com>
2024-08-22 23:16:31 +0545

    adding all files to the repository

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+     /*   //variables
+       // int age=30;
+        //int myAge=age;
+        String message="Hello";
+//        System.out.println("myage"+myAge);
+        System.out.println(message);
+        System.out.println(message.length());*/
+        int arr[]=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(arr);
+        }
+    }
+}
